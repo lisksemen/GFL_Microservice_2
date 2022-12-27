3b45bbce3c356bd1b4f7573d04e1fffd855a0b02
lisak <lisk.semen@gmail.com>
2022-12-27 23:18:10 +0100

    Initial commit

---
+++ src/main/java/group/executor/GflMicroservice2Application.java
@@ -0,0 +1,13 @@
+package group.executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GflMicroservice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GflMicroservice2Application.class, args);
+    }
+
+}
---
+++ src/test/java/group/executor/GflMicroservice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package group.executor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GflMicroservice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
