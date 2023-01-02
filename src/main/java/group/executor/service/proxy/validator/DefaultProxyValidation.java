10e9458f9df433673ace4bc9a48deab11a2824aa
Essur <asus15oleg@gmail.com>
2023-01-02 14:39:46 +0200

    Switch squareup.okhttp to okhttp3 in pom.xml Refactor DefaultProxyValidation to okhttp3 Refactor test for work with request for new proxy

--- src/main/java/group/executor/service/proxy/validator/DefaultProxyValidation.java
+++ src/main/java/group/executor/service/proxy/validator/DefaultProxyValidation.java
@@ -2,11 +2,14 @@ package group.executor.service.proxy.validator;
 
 import group.executor.model.ProxyConfigHolder;
 import group.executor.model.ProxyNetworkConfig;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.stereotype.Service;
-import com.squareup.okhttp.*;
 
 import java.io.IOException;
 import java.net.*;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class DefaultProxyValidation implements ProxyValidation {
@@ -14,17 +17,22 @@ public class DefaultProxyValidation implements ProxyValidation {
     public boolean validateProxy(ProxyConfigHolder proxyConfigHolder) {
         if (proxyConfigHolder != null){
             try {
-                OkHttpClient client = new OkHttpClient();
                 ProxyNetworkConfig proxyNetworkConfig = proxyConfigHolder.getProxyNetworkConfig();
-                URL url = new URL("https://2ip.io/ua/");
-                Request request = new Request.Builder()
-                        .url(url)
-                        .build();
                 Proxy proxy = new Proxy(Proxy.Type.HTTP,
                         new InetSocketAddress(proxyNetworkConfig.getHostName(),proxyNetworkConfig.getPort()));
-                Call call = client.setProxy(proxy).newCall(request);
-                if (call.execute().isSuccessful()){
-                    return true;
+                OkHttpClient client = new OkHttpClient.Builder()
+                        .proxy(proxy)
+                        .readTimeout(15, TimeUnit.SECONDS)
+                        .build();
+                URL url = new URL("https://www.google.com");
+                Request request = new Request.Builder()
+                        .get()
+                        .url(url)
+                        .build();
+                try (Response response = client.newCall(request).execute()) {
+                    if (response.isSuccessful()) {
+                        return true;
+                    }
                 }
             } catch (IOException e) {
                 return false;
---
+++ src/test/java/group/executor/service/proxy/validation/DefaultProxyValidationTest.java
@@ -0,0 +1,39 @@
+package group.executor.service.proxy.validation;
+
+import group.executor.model.ProxyConfigHolder;
+import group.executor.model.ProxyCredentials;
+import group.executor.model.ProxyNetworkConfig;
+import group.executor.service.handler.DefaultProxySourceQueueHandler;
+import group.executor.service.handler.ProxySourceQueueHandler;
+import group.executor.service.proxy.sourceUrl.DefaultProxySourceUrl;
+import group.executor.service.proxy.sourceUrl.ProxySourceUrl;
+import group.executor.service.proxy.validator.DefaultProxyValidation;
+import group.executor.service.proxy.validator.ProxyValidation;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DefaultProxyValidationTest {
+    private ProxyValidation proxyValidation;
+    private ProxyConfigHolder proxyConfigHolderNotWorking;
+    private ProxySourceUrl proxySourceUrl;
+    private ProxySourceQueueHandler proxySourceQueueHandler;
+    @BeforeEach
+    void setUp() {
+        proxySourceQueueHandler = new DefaultProxySourceQueueHandler();
+        proxySourceUrl = new DefaultProxySourceUrl(proxySourceQueueHandler);
+        proxyConfigHolderNotWorking = new ProxyConfigHolder(new ProxyNetworkConfig("103.248.120.5",8),
+                new ProxyCredentials());
+        proxyValidation = new DefaultProxyValidation();
+    }
+
+    @Test
+    public void validateProxy(){
+        proxySourceUrl.sendRequest();
+        boolean result1 = proxyValidation.validateProxy(proxySourceQueueHandler.pollProxy().get());
+        boolean result2 = proxyValidation.validateProxy(proxyConfigHolderNotWorking);
+        assertTrue(result1);
+        assertFalse(result2);
+    }
+}
