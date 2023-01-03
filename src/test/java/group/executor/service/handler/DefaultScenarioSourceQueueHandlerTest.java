6a40a7d88dab305037fcbd8a99426143749e5a5b
OlehNoskov <olega6000@gmail.com>
2023-01-03 13:23:15 +0200

    Create DefaultProxySourceQueueHandlerTest and refactor DefaultScenarioSourceQueueHandlerTest

--- src/test/java/group/executor/service/handler/DefaultScenarioSourceQueueHandlerTest.java
+++ src/test/java/group/executor/service/handler/DefaultScenarioSourceQueueHandlerTest.java
@@ -1,33 +1,41 @@
 package group.executor.service.handler;
 
 import group.executor.model.Scenario;
+import group.executor.model.Step;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Queue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 
 class DefaultScenarioSourceQueueHandlerTest {
 
-    private final Queue<Scenario> scenarios = new LinkedBlockingQueue<>();
+    private DefaultScenarioSourceQueueHandler defaultScenarioSourceQueueHandler;
     private Scenario scenario;
 
     @BeforeEach
     public void setUp() {
-        scenario = new Scenario();
-    }
+        defaultScenarioSourceQueueHandler = new DefaultScenarioSourceQueueHandler();
+        Step step = new Step("sleep", "3");
+        List<Step> steps = new ArrayList<>();
+        steps.add(step);
+        scenario = new Scenario("test", "testSite", steps);
 
-    @Test
-    public void addScenario() {
-        scenarios.add(scenario);
-        Assertions.assertEquals(1, scenarios.size());
+        defaultScenarioSourceQueueHandler.addScenario(scenario);
     }
 
     @Test
     void pollScenario() {
-        scenarios.poll();
-        Assertions.assertEquals(0, scenarios.size());
+        Optional<Scenario> optionalScenario = defaultScenarioSourceQueueHandler.pollScenario();
+
+        Assertions.assertEquals("test", optionalScenario.get().getName());
+        Assertions.assertEquals("testSite", optionalScenario.get().getSite());
+
+        Assertions.assertEquals("sleep", optionalScenario.get().getSteps().get(0).getAction());
+        Assertions.assertEquals("3", optionalScenario.get().getSteps().get(0).getValue());
     }
 }
