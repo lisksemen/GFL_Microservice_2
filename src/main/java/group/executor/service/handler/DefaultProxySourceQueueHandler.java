3733bdd7e92fd709001136973fdf000f9862cb62
VassBassApp <vassbassapp@gmail.com>
2023-01-04 17:56:17 +0200

    Fix

--- src/main/java/group/executor/service/handler/DefaultProxySourceQueueHandler.java
+++ src/main/java/group/executor/service/handler/DefaultProxySourceQueueHandler.java
@@ -3,6 +3,7 @@ package group.executor.service.handler;
 import group.executor.model.ProxyConfigHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.Optional;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -13,8 +14,8 @@ public class DefaultProxySourceQueueHandler implements ProxySourceQueueHandler {
     private final Queue<ProxyConfigHolder> proxyQueue = new LinkedBlockingQueue<>();
 
     @Override
-    public void addProxy(ProxyConfigHolder proxyConfigHolder) {
-        proxyQueue.add(proxyConfigHolder);
+    public void addProxy(ProxyConfigHolder ... proxyConfigHolder) {
+        Collections.addAll(proxyQueue, proxyConfigHolder);
     }
 
     @Override
