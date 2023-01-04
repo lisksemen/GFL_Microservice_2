ee5c981ff0761aa73923db4900ba14e6fb30977d
Essur <asus15oleg@gmail.com>
2023-01-04 19:17:54 +0200

    Add public method access of objectMapper in ObjectMapperConfig

--- src/main/java/group/executor/config/ObjectMapperConfig.java
+++ src/main/java/group/executor/config/ObjectMapperConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ObjectMapperConfig {
     @Bean
-    ObjectMapper objectMapper(){
+    public ObjectMapper objectMapper(){
         return new ObjectMapper();
     }
 }
