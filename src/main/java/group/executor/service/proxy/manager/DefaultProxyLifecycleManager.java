56ac8d034511bebe5d14ff3ebd1902cde0cce20e
lisak <lisk.semen@gmail.com>
2023-01-05 15:48:08 +0100

    Added LifecycleManager for proxy

---
+++ src/main/java/group/executor/service/proxy/manager/DefaultProxyLifecycleManager.java
@@ -0,0 +1,36 @@
+package group.executor.service.proxy.manager;
+
+import group.executor.model.ProxyConfigHolder;
+import group.executor.service.handler.ProxySourceQueueHandler;
+import group.executor.service.proxy.validator.ProxyValidator;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class DefaultProxyLifecycleManager implements ProxyLifecycleManager {
+
+    private final ProxySourceQueueHandler proxySourceQueueHandler;
+
+    private final ProxyValidator proxyValidator;
+
+    @Override
+    public void removeInvalidProxy() {
+        // TODO
+    }
+
+    @Override
+    public Optional<ProxyConfigHolder> getFirstValidProxy() {
+        synchronized (proxySourceQueueHandler) {
+            Optional<ProxyConfigHolder> proxyOptional;
+            while ((proxyOptional = proxySourceQueueHandler.pollProxy()).isPresent()) {
+                ProxyConfigHolder proxy = proxyOptional.get();
+                if (proxyValidator.isValid(proxy))
+                    return Optional.of(proxy);
+            }
+            return Optional.empty();
+        }
+    }
+}
