486486202984db99c080f5b517ebfabdbc57926a
Essur <asus15oleg@gmail.com>
2023-01-06 15:17:26 +0200

    Fix

--- src/main/java/group/executor/service/proxy/sourceUrl/DefaultProxySourceUrl.java
+++ src/main/java/group/executor/service/proxy/sourceUrl/DefaultProxySourceUrl.java
@@ -50,13 +50,13 @@ public class DefaultProxySourceUrl implements ProxySourceUrl {
                     .url(url)
                     .build();
             Response response = client.newCall(request).execute();
-            if (response.isSuccessful()){
-                proxySourceQueueHandler.addProxy(getProxyFromResponse(response.body().string()));
-            }
             LOGGER.info("ResponseBodyToString: " + response.body().string());
             LOGGER.info("-------------------------------------------------");
             LOGGER.info("ProxyFromResponse: " + getProxyFromResponse(response.body().string()));
             LOGGER.info("-------------------------------------------------");
+            if (response.isSuccessful()){
+                proxySourceQueueHandler.addProxy(getProxyFromResponse(response.body().string()));
+            }
         } catch (IOException e) {
             throw new RuntimeException("Message: " + e.getMessage());
         }
